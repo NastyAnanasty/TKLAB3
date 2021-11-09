678076a4dd4461929512207df8de705ffbeac54d
vladislav.selitskii <vladislav.selitskii@piano.io>
2021-11-09 23:31:15 +0400

    3.2 completed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import random
+
 import numpy as np
+
+import lab2
 
 
 def all_word(r):
@@ -23,24 +27,81 @@ def all_word(r):
 def gen_matr(r):
     gen_matr = np.eye(all_word(r).shape[0])
     gen_matr = np.hstack((gen_matr, all_word(r)))
-    print(gen_matr)
+    #print(gen_matr)
     return gen_matr
 
 
 def check_matr(r):
     check_matr = np.eye(r)
     check_matr = np.vstack((all_word(r), check_matr))
-    print(check_matr)
+    #print(check_matr)
     return check_matr
 
 
-def syndrom(r):
+def get_code_word(matrix, k):
+    k_word = []
+    for i in range(k):
+        k_word.append(lab2.round_num(random.uniform(0, 1)))
+    n_word = lab2.code_word_from_k_to_n(matrix, k_word) % 2
+    return n_word
+
+
+def get_syndrom(r):
     return check_matr(r)
 
 
+def task3_2(r, error_num):
+    n = 2 ** r - 1
+    k = 2 ** r - r - 1
+    g_matrix = gen_matr(r)
+    h_matrix = check_matr(r)
+    syndrom_matrix = get_syndrom(r)
+
+    n_word = get_code_word(g_matrix, k)
+    n_word_with_mistake = []
+    if error_num == 1:
+        n_word_with_mistake = lab2.make_single_mistake_in_n_word(n_word) % 2
+    if error_num == 2:
+        n_word_with_mistake = lab2.make_double_mistake_in_n_word(n_word) % 2
+    if error_num == 3:
+        n_word_with_mistake = lab2.make_triple_mistake_in_n_word(n_word) % 2
+
+    syndrom = n_word_with_mistake @ h_matrix % 2
+    row_num = lab2.row_index_in_matrix(syndrom, syndrom_matrix)
+    if row_num == -1:
+        print("Синдром не найден для r = " + str(r) + " и "
+              + str(error_num) + " ошибки")
+    else:
+        errors_table = np.eye(n, dtype=int)
+        error = errors_table[row_num]
+        may_be_n_word = np.abs(n_word_with_mistake - error)
+        if np.array_equiv(n_word, may_be_n_word):
+            print("Ошибка исправлена")
+        else:
+            print("Ошибка не исправлена")
+
+
 if __name__ == '__main__':
-    r = 4
-    my = gen_matr(r)
-    my = check_matr(r)
-    ny=syndrom(r)
-    print(ny)
+    #this is a print for 3.1 task
+    # r = 4
+    # my = gen_matr(r)
+    # my = check_matr(r)
+    # ny = get_syndrom(r)
+    # print(ny)
+
+    task3_2(2, 1)
+    task3_2(2, 2)
+    task3_2(2, 3)
+
+    task3_2(3, 1)
+    task3_2(3, 2)
+    task3_2(3, 3)
+
+    task3_2(4, 1)
+    task3_2(4, 2)
+    task3_2(4, 3)
+
+
+
+
+
